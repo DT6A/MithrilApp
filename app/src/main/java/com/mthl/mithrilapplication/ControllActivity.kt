cced7404414fbddfed63c567d779e32e7ad1ce18
Тарасов Денис <dt6cgsg@gmail.com>
2020-04-02 17:33:39 +0300

    Version 0.0.2
    -- Add receiving bytes from microcontroller to the Command activity

--- app/src/main/java/com/mthl/mithrilapplication/ControllActivity.kt
+++ app/src/main/java/com/mthl/mithrilapplication/ControllActivity.kt
@@ -1,25 +1,72 @@
 package com.mthl.mithrilapplication
 
+import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_controll.*
 import java.lang.Exception
 
 class ControllActivity : AppCompatActivity() {
+        private var isOn = true
+        private class bluetoothReciever(c: Context, val notifier: ControllActivity.notifRecieve) : AsyncTask<Void, Void, String>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_controll)
+            private var context: Context
 
-        sendCommandBtn.setOnClickListener {
-            if (BluetoothActivity.m_bluetoothSocket != null) {
-                try {
-                    BluetoothActivity.m_bluetoothSocket!!.outputStream.write(commandET.text.toString().toByteArray())
+            init {
+                this.context = c
+            }
+
+            override fun onPreExecute() {
+                super.onPreExecute()
+            }
+
+            override fun doInBackground(vararg params: Void?): String {
+                while ((context as ControllActivity).isOn) {
+                    try {
+                        Thread.sleep(500)
+                        val data: ByteArray = ByteArray(100)
+                        val len = BluetoothActivity.m_bluetoothSocket!!.inputStream.read(data)
+                        notifier.showData(data.sliceArray(0..(len - 1)))
+                    }
+                    catch (e: Exception) {
+
+                    }
                 }
-                catch (e: Exception) {
-                    e.printStackTrace()
-                }
+
+                return null.toString()
             }
         }
+
+
+        private class notifRecieve(val c: ControllActivity) {
+            fun showData(data: ByteArray) {
+                c.dataTV.setText(data.toString(Charsets.US_ASCII))
+            }
+        }
+
+        private lateinit var btTask: bluetoothReciever
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_controll)
+
+            sendCommandBtn.setOnClickListener {
+                if (BluetoothActivity.m_bluetoothSocket != null) {
+                    try {
+                        BluetoothActivity.m_bluetoothSocket!!.outputStream.write(commandET.text.toString().toByteArray())
+                    }
+                    catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            btTask = bluetoothReciever(this, notifRecieve(this))
+            btTask.execute()
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isOn = false
     }
 }
