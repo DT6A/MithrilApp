46c0192d472aa15933dabcaddfb52a00579cf543
Тарасов Денис <dt6cgsg@gmail.com>
2020-05-19 12:06:29 +0300

    Version 0.1.0 -- Control activity inerface changed. Added calibraion button and posture processing switch bar. -- Control activity now sends compatible bytes for calibration and posture processing.

--- app/src/main/java/com/mthl/mithrilapplication/ControllActivity.kt
+++ app/src/main/java/com/mthl/mithrilapplication/ControllActivity.kt
@@ -1,5 +1,6 @@
 package com.mthl.mithrilapplication
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,17 @@ import kotlinx.android.synthetic.main.activity_controll.*
 import java.lang.Exception
 
 class ControllActivity : AppCompatActivity() {
+
+        private val CALIBRATION_BYTE: Char = 'C'
+        private val POSTURE_ON_BYTE: String = "P"
+        private val POSTURE_OFF_BYTE: String = "D"
+
+        companion object {
+            var isPostureOn:Boolean = true
+            var isCalibrationRunning:Boolean = false
+            lateinit var progress: ProgressDialog
+        }
+
         private var isOn = true
         private class bluetoothReciever(c: Context, val notifier: ControllActivity.notifRecieve) : AsyncTask<Void, Void, String>() {
 
@@ -22,9 +34,10 @@ class ControllActivity : AppCompatActivity() {
             }
 
             override fun doInBackground(vararg params: Void?): String {
-                while ((context as ControllActivity).isOn) {
+                while (!isCancelled() and (context as ControllActivity).isOn) {
+                    while (isCalibrationRunning) {}
                     try {
-                        Thread.sleep(500)
+                        Thread.sleep(100)
                         val data: ByteArray = ByteArray(100)
                         val len = BluetoothActivity.m_bluetoothSocket!!.inputStream.read(data)
                         notifier.showData(data.sliceArray(0..(len - 1)))
@@ -38,14 +51,48 @@ class ControllActivity : AppCompatActivity() {
             }
         }
 
+        private lateinit var btTask: bluetoothReciever
+        private class calibrationReciever(c: Context) : AsyncTask<Void, Void, String>() {
 
+            private var context: Context
+
+            init {
+                this.context = c
+            }
+
+            override fun onPreExecute() {
+                super.onPreExecute()
+                isCalibrationRunning = true
+                BluetoothActivity.m_bluetoothSocket!!.outputStream.write((context as ControllActivity).CALIBRATION_BYTE.toString().toByteArray())
+                progress = ProgressDialog.show(context, "Calibrating", "Please stay still")
+            }
+
+            override fun doInBackground(vararg params: Void?): String {
+                val tBegin = System.currentTimeMillis()
+
+                while (System.currentTimeMillis() - tBegin < 4000) {
+                    val data: ByteArray = ByteArray(100)
+                    val len = BluetoothActivity.m_bluetoothSocket!!.inputStream.read(data)
+                    if (data.indexOf((context as ControllActivity).CALIBRATION_BYTE.toByte()) != -1)
+                        break
+                }
+
+                return null.toString()
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                isCalibrationRunning = false
+                progress.dismiss()
+            }
+        }
         private class notifRecieve(val c: ControllActivity) {
             fun showData(data: ByteArray) {
                 c.dataTV.setText(data.toString(Charsets.US_ASCII))
             }
         }
 
-        private lateinit var btTask: bluetoothReciever
+        private lateinit var calibrTask: calibrationReciever
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -61,8 +108,35 @@ class ControllActivity : AppCompatActivity() {
                     }
                 }
             }
+
+            if (isPostureOn)
+                switchPosture.isChecked = true
+            switchPosture.setOnCheckedChangeListener { _, isChecked ->
+                isPostureOn = isChecked
+                if (BluetoothActivity.m_bluetoothSocket != null) {
+                    try {
+                        var v: String = if (isChecked) POSTURE_ON_BYTE else POSTURE_OFF_BYTE
+                        BluetoothActivity.m_bluetoothSocket!!.outputStream.write(v.toByteArray())
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+
             btTask = bluetoothReciever(this, notifRecieve(this))
-            btTask.execute()
+            btTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+
+            calibrCommandBtn.setOnClickListener {
+                if (BluetoothActivity.m_bluetoothSocket != null) {
+                    try {
+                        calibrTask = calibrationReciever(this)
+                        calibrTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+                    }
+                    catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
         }
 
     override fun onDestroy() {
